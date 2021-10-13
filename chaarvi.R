05d1b91b98c9445378f6317994dd4a16624665b9
Chaarvi-i <92332538+Chaarvi-i@users.noreply.github.com>
2021-10-14 01:46:21 +0530

    Update chaarvi.R

--- chaarvi.R
+++ chaarvi.R
@@ -1,6 +1,6 @@
-a = "Chaarvi"
+a = "Chaarvi Narang"
 b = "narang.chaarvi@gmail.com"
 c = "@chaarvi"
 d = "genomics"
-e = "r"
+e = "R"
 sprintf("my name is %s! my email id is %s! my slack username is %s! my biostack is %s! I am writing using %s! script",a,b,c,d,e)
