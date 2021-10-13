a6aa973f2219d7abbfc949435b45e1dec1e83782
Ugogabby <92388596+Ugogabby@users.noreply.github.com>
2021-10-14 00:59:35 +0100

    Add files via upload

---
+++ Gabby.py
@@ -0,0 +1,6 @@
+name ="ezigbo ugochukwu gabriel"
+email = "ezeigbougochukwu@gmail.com"
+slack_username = "ugoGabby"
+biostack = "Bioinformatics/drug design and developement"
+
+print("My name is",name,". "My email is",email,". "My slack username is",slack_username,". "My biostack is",biostack,".)
