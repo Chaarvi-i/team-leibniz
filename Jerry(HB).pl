501410cfb65537326dd94afd219d6a45346d80c8
jillurinz <87633445+jillurinz@users.noreply.github.com>
2021-10-13 15:51:42 +0530

    Add files via upload

---
+++ Jerry(HB).pl
@@ -0,0 +1,8 @@
+#HackBio Stage0 
+
+$name= "JERRY JOHN";
+$emailid= "jerryjohn1999\@gmail.com";
+$slack= "\@Jerry";
+$biostack= "Drug designing and development";
+
+print ("Name: $name\nEmailId: $emailid\nSlack Username: $slack\nBiostack: $biostack\n");
