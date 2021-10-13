0fc988b33122de1f42da03537721cd8e3b59517d
mitykay <47569280+mitykay@users.noreply.github.com>
2021-10-14 03:31:34 +0800

    Add files via upload

---
+++ kayode task1.py
@@ -0,0 +1,7 @@
+# Hackbio Stage 0
+name="Kayode Raheem"
+email="raheemkayode2017@gmail.com"
+slack_username="@mitykay"
+biostack="drug design/development"
+
+print("My name is",name,". My email is ",email,". My slack_username is", slack_username,". My biostack is", biostack,".")
