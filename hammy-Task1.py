e39c006f5fade8a11211e440362dc0d63718a30b
hamiq <92406097+hamiq@users.noreply.github.com>
2021-10-13 12:09:42 -0400

    Add files via upload

---
+++ hammy-Task1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+name ="Hamail Iqbal"
+email = "hi70@scarletmail.rutgers.edu"
+slack_username = "hammy"
+biostack = "Machine Learning/Bioinformatics"
+
+print("My name is", name, '\n' "My email is", email, '\n' "My slack username is", slack_username, '\n' "My biostack is", biostack)
+
