31c844d18200f438ef01039f39c5a0dabc1637d6
Chaarvi-i <92332538+Chaarvi-i@users.noreply.github.com>
2021-10-14 17:04:40 +0530

    Update Script_Chaarvi.R

--- Script_Chaarvi.R
+++ Script_Chaarvi.R
@@ -1,6 +1,4 @@
-a = "Chaarvi Narang"
-b = "narang.chaarvi@gmail.com"
-c = "@chaarvi"
-d = "genomics"
-e = "R"
-sprintf("my name is %s! my email id is %s! my slack username is %s! my biostack is %s! I am writing using %s! script",a,b,c,d,e)
+print("Name: Chaarvi Narang")
+print("Email ID: narang.chaarvi@gmail.com")
+print("Slack username: @chaarvi")
+print("Biostack: Genomics")
