60bdbd2c84c6e4d1815deb0229ef518d6d1c91aa
jillurinz <87633445+jillurinz@users.noreply.github.com>
2021-10-15 15:21:32 +0530

    Update Script_Jerry.pl

--- Script_Jerry.pl
+++ Script_Jerry.pl
@@ -4,5 +4,4 @@ $name= "JERRY JOHN";
 $emailid= "jerryjohn1999\@gmail.com";
 $slack= "\@Jerry";
 $biostack= "Drug designing and development";
-
-print ("Name: $name\nEmailId: $emailid\nSlack Username: $slack\nBiostack: $biostack\n");
+print ($name,$emailid,$slack,$biostack);
