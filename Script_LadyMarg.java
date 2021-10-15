7e6c7825f453dda57805336bcfe71fc49251d922
LadyMarg <adeogunmargaret@gmail.com>
2021-10-15 09:35:42 +0100

    updated ladymarg

--- Script_LadyMarg.java
+++ Script_LadyMarg.java
@@ -1,12 +1,9 @@
-package com.company;
-
 public class Main {
 
     public static void main(String[] args) {
-	   //write your code here
         System.out.println("Margaret Adedayo Opeoluwa, " +
                             "adeogunmargaret@gmail.com, " +
                             "@LadyMarg, " +
-                            "Microbiology.");
+                            "Microbiology");
     }
 }
