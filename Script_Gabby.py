20c316020c831db95166acdfb903fe56219286de
Chaarvi-i <92332538+Chaarvi-i@users.noreply.github.com>
2021-10-15 15:09:10 +0530

    Update Script_Gabby.py

--- Script_Gabby.py
+++ Script_Gabby.py
@@ -3,4 +3,4 @@ email = "ezeigbougochukwu@gmail.com"
 slack_username = "ugoGabby"
 biostack = "Bioinformatics/drug design and developement"
 
-print("My name is",name,". "My email is",email,". "My slack username is",slack_username,". "My biostack is",biostack,".)
+print(name,email,slack_username,biostack)
