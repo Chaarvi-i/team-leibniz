2b570b4507a58eeea489b8416e2d4eeabc4f5838
jillurinz <87633445+jillurinz@users.noreply.github.com>
2021-10-16 01:50:34 +0530

    Update Script_Jerry.pl

--- Script_Jerry.pl
+++ Script_Jerry.pl
@@ -1,8 +1,7 @@
 #HackBio Stage0 
 
-#$name= "JERRY JOHN";
-#$emailid= "jerryjohn1999\@gmail.com";
-#$slack= "\@Jerry";
-#$biostack= "Drug designing and development";
-#print ($name,$emailid,$slack,$biostack);
-print ("Jerry John","jerryjohn1999\@gmail.com","\@Jerry","Drug designing and development",);
+$name= "JERRY JOHN";
+$emailid= "jerryjohn1999\@gmail.com";
+$slack= "\@Jerry";
+$biostack= "Drug designing and development";
+print ($name,$emailid,$slack,$biostack);
