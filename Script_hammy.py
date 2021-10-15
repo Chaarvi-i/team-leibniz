f6cfb9b32c596dedce7bc3a7b975be1bdc7527ce
hamiq <92406097+hamiq@users.noreply.github.com>
2021-10-15 08:05:53 -0400

    Update Script_hammy.py

--- Script_hammy.py
+++ Script_hammy.py
@@ -3,4 +3,4 @@ email = "hi70@scarletmail.rutgers.edu"
 slack_username = "hammy"
 biostack = "Machine Learning/Bioinformatics"
 
-print("My name is", name, '\n' "My email is", email, '\n' "My slack username is", slack_username, '\n' "My biostack is", biostack)
+print(name, email, slack_username, biostack)
