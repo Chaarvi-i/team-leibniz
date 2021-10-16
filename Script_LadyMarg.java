0c9998f3c2bd681e76942fee745efbb3c0749c39
jillurinz <87633445+jillurinz@users.noreply.github.com>
2021-10-16 10:48:02 +0530

    Update Script_LadyMarg.java

--- Script_LadyMarg.java
+++ Script_LadyMarg.java
@@ -1,4 +1,4 @@
-public class HBSTAGE0 {
+public class Script_LadyMarg {
     public static void main(String[] args) {
         System.out.println("Margaret Adedayo Opeoluwa,adeogunmargaret@gmail.com,@LadyMarg,Microbiology");
     }
